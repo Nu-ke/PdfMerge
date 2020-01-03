97c9389eac973f62950fb12084cfaec011838ced
Louis Constantin <l.constantin@abc-arbitrage.com>
2020-01-03 11:32:55 +0100

    Bootstrap merger

--- PdfMerge/PdfMerge.py
+++ PdfMerge/PdfMerge.py
@@ -1,1 +1,10 @@
+import sys
+from pdfMerge.Merge import pdfMerger
 
+folderPath = sys.argv[1]
+print ("Merging pdf in folder %s" % folderPath)
+
+merger = pdfMerger.pdfMerger(folderPath)
+merger.merge()
+
+print("Merged")
---
+++ PdfMerge/pdfMerge/Merge/pdfMerger.py
@@ -0,0 +1,8 @@
+class pdfMerger():
+    
+	def __init__(self, mergePath):
+			  self.mergePath = mergePath
+
+	def merge(self):
+		pass
+
